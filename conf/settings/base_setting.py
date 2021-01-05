4d5602fed0133d4afb2be10e0f8352deccac3896
uibeom96 <dmlqja7570@daum.net>
2021-01-05 17:46:11 +0900

    debug_toolbar 적용완료 쿼리 중복확인에 필요

--- conf/settings/base_setting.py
+++ conf/settings/base_setting.py
@@ -13,7 +13,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
+
+INTERNAL_IPS = ('127.0.0.1')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -23,6 +26,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'conf.urls'
--- conf/settings/develop.py
+++ conf/settings/develop.py
@@ -22,3 +22,5 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+INTERNAL_IPS = ('127.0.0.1')
